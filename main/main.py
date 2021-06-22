9659be9d86ee1b7908f2d8edf072767f6efac921
Nik11021999 <85795971+Nik11021999@users.noreply.github.com>
2021-06-22 18:14:01 +0200

    Main potente

--- main/main.py
+++ main/main.py
@@ -1,92 +1,49 @@
-import time
-import random
-from Libraries import library_euristica as eur, library_CVRPTW as lib
+from librerie import libreria_Alg_Genetico as gen, libreria_CVRPTW as lib
 
-file = "C101.txt"
+file = "..\Solomon\C101.txt"
 istanza = lib.CVRPTW(file)
 
 
-def BCRC(soluzione1, soluzione2, range):
 
-    # Seed per le funzioni randomiche
-    random.seed(time.time())
-
-    # Si rimuove il deposito dai genitori
-    soluzione1.routes = [i for i in soluzione1.routes if i.number != 0]
-    soluzione2.routes = [i for i in soluzione2.routes if i.number != 0]
-
-    # Si prendono due indici casuali in un range predefinito
-    index1 = random.randint(0, len(soluzione1.routes)-range)
-    index2 = random.randint(index1, index1+range)
-
-    # Si costruiscono i tagli sulla base dei due indici casuali
-    list1 = soluzione1.routes[index1:index2]
-    list2 = soluzione2.routes[index1:index2]
-
-    # Si rimuovono le liste dai genitori
-    for j in list2:
-        soluzione1.routes = [i for i in soluzione1.routes if i.number != j.number]
-
-    for j in list1:
-        soluzione2.routes = [i for i in soluzione2.routes if i.number != j.number]
-
-    # Si calcolano le distanze dei nodi nel parente 1 per ogni nodo nella lista 2
-    results = []
-    # Dizionario del tipo:
-    # Key -> Numero di ogni nodo i-esimo
-    # Valore -> distanza calcolata del nodo i-esimo di parent dal nodo j-esimo di lista
-    distances_from_list2 = {}
-    for j in list2:
-        temp = []
-        for i in soluzione1.routes:
-            # Si calcolano le distanze per ogni nodo j ed i
-            distances_from_list2[i.number] = (soluzione1.istanza.nodes[j.number].distance(soluzione1.istanza.nodes[i.number]))
-        # Si prende la chiave associata al minimo di queste distanze per capire dove inserire il nodo j di list2
-        temp = min(distances_from_list2.values())
-        # Si ha la lista dei nodi più vicini, possono essere più di uno
-        results = ([key for key in distances_from_list2 if distances_from_list2[key] == temp])
-        # Bisogna ora trovare l'indice del minimo. Se ce n'è più di uno, se ne sceglie uno soltanto randomicamente
-        indice = 0
-        for i in soluzione1.routes:
-            if i.number == results[random.randint(0, len(results)-1)]:
-                indice = soluzione1.routes.index(i)
-                break
-        # Si inserisce il minimo all'indice calcolato
-        soluzione1.routes.insert(indice+1, j)
-
-    results = []
-    distances_from_list1 = {}
-    for j in list1:
-        temp = []
-        for i in soluzione2.routes:
-            distances_from_list1[i.number] = (soluzione2.istanza.nodes[j.number].distance(soluzione2.istanza.nodes[i.number]))
-        temp = min(distances_from_list1.values())
-        results = ([key for key in distances_from_list1 if distances_from_list1[key] == temp])
-        for i in soluzione2.routes:
-            if i.number == results[random.randint(0, len(results) - 1)]:
-                indice = soluzione2.routes.index(i)
-                break
-        soluzione2.routes.insert(indice+1, j)
-
-    return soluzione1, soluzione2
-
+# ---------------------------Generazione della popolazione di soluzioni iniziali ------------------------------
 
 def main():
-    test = eur.Algoritmo_genetico(istanza)
-    test.gen_popolazione_iniziale()
 
-    soluzione1 = test.popolazione[random.randint(0, len(test.popolazione))]
-    soluzione2 = test.popolazione[random.randint(0, len(test.popolazione))]
+    # soluzione = istanza.optimize()
+    test = gen.Algoritmo_genetico(istanza)
+    popolazione = test.gen_popolazione_iniziale()
 
-
-    for i in range(0, 4):
-        crossover1, crossover2 = (BCRC(soluzione1, soluzione2, 20))
-
-        if crossover1.is_admissible() and crossover2.is_admissible():
-            print("Crossover ammissibili")
-        else:
-            print("Crossover inammissibili")
+    print(popolazione[0].valore_f_ob)
+    print(popolazione[49].valore_f_ob)
+    i = 0
+    for _ in range(50):
+        (sol1, sol2) = test.test()
+        sol1.is_admissible()
+        sol2.is_admissible()
+        if sol1.truck_count <= 25 :
+            print(i+1, " ", sol1.valore_f_ob, " : ", sol1.truck_count, "||| ", sol2.valore_f_ob, " : ", sol2.truck_count)
+            i += 1
 
 
 if __name__ == "__main__":
     main()
+
+"""
+Graph2 = lib.nx.DiGraph()
+list_nodes = list(range(istanza.num_nodes))
+Graph2.add_nodes_from(list_nodes)
+for route in routes:
+        for j in range(len(route)-1):
+            Graph2.add_edge(route[j], route[j+1])
+
+coordinate = {}
+for i in range(istanza.num_nodes):
+    coordinate[i] = istanza.nodes[i].coordinate
+
+lib.nx.draw_networkx(Graph2, coordinate, font_size=8, font_color='k', node_color='green', edgecolors='k',
+                 node_size=200)
+lib.nx.draw_networkx_edges(Graph2, coordinate, arrowsize=7, arrowstyle='->', edge_color='black')
+#lib.plt.show()
+
+#istanza.print_solution(soluzione[0])
+"""
