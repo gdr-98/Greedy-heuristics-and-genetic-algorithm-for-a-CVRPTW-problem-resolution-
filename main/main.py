6502e901cf4e91201df0e4cab812a24bd615a167
Nik11021999 <85795971+Nik11021999@users.noreply.github.com>
2021-06-23 19:41:28 +0200

    Update main.py

--- main/main.py
+++ main/main.py
@@ -3,8 +3,6 @@ from librerie import libreria_Alg_Genetico as gen, libreria_CVRPTW as lib
 file = "..\Solomon\C101.txt"
 istanza = lib.CVRPTW(file)
 
-
-
 # ---------------------------Generazione della popolazione di soluzioni iniziali ------------------------------
 
 def main():
@@ -12,31 +10,49 @@ def main():
     # soluzione = istanza.optimize()
     test = gen.Algoritmo_genetico(istanza)
     popolazione = test.gen_popolazione_iniziale()
+    best = test.best_solution()
+
     best = test.start_algorithm()
-    print(best)
     for i in test.popolazione:
         print(i.valore_f_ob )
-
+    print(best)
 
 if __name__ == "__main__":
-    main()
+    pass
+main()
 
-"""
-Graph2 = lib.nx.DiGraph()
-list_nodes = list(range(istanza.num_nodes))
-Graph2.add_nodes_from(list_nodes)
-for route in routes:
-        for j in range(len(route)-1):
-            Graph2.add_edge(route[j], route[j+1])
+"""    
 
-coordinate = {}
-for i in range(istanza.num_nodes):
-    coordinate[i] = istanza.nodes[i].coordinate
+best = gen.Soluzione([istanza.nodes[1],istanza.nodes[2],istanza.nodes[4],istanza.nodes[5],istanza.nodes[6],istanza.nodes[7]], istanza, 0)
+    worst = gen.Soluzione([istanza.nodes[7], istanza.nodes[1], istanza.nodes[6], istanza.nodes[2], istanza.nodes[4], istanza.nodes[5]],istanza, 0)
+    son1,son2 = test.double_crossover(best,worst,4)
 
-lib.nx.draw_networkx(Graph2, coordinate, font_size=8, font_color='k', node_color='green', edgecolors='k',
-                 node_size=200)
-lib.nx.draw_networkx_edges(Graph2, coordinate, arrowsize=7, arrowstyle='->', edge_color='black')
-#lib.plt.show()
+    genitore_1 = [i.number for i in best.routes]
+    genitore_2 = [i.number for i in worst.routes]
 
-#istanza.print_solution(soluzione[0])
+    print("Genitore 1 :",genitore_1)
+    print("Genitore 2 :", genitore_2)
+
+    lista_nodi1 = [i.number for i in son1.routes]
+    lista_nodi2 = [i.number for i in son2.routes]
+
+    print("Sol 1 : ", lista_nodi1)
+    print("Sol 2 : ", lista_nodi2)
+    visto = []
+    for i in lista_nodi1:
+        if i in visto:
+            print("*****************ERRORE : ", i)
+
+        else:
+            visto.append(i)
+    print(visto)
+    visto = []
+
+    for i in lista_nodi2:
+        if i in visto:
+            print("*****************ERRORE : ", i)
+        else:
+            visto.append(i)
+    print(visto)
+
 """
